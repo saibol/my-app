e15834e86ed9060eac27c62c58fb2d96ff7320e7
Sunil Saibol <sunil_saibol@yahoo.com>
2025-01-15 22:48:29 -0500

    added empty commit

--- cypress.config.js
+++ cypress.config.js
@@ -11,7 +11,8 @@ module.exports = defineConfig({
 
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implement node event listeners here  
+      
     },
   },
 });
